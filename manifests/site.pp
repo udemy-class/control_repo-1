c96a42cddb5bfa715ef8346a1eb3cdf0a88d9ef7
pinejunction <54043253+pinejunction@users.noreply.github.com>
2019-08-19 17:09:31 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,7 +2,11 @@ node default {
   }
 node 'master.puppet.vm' {
   include role::master_server
-    }
+  file  { '/root/README':
+  ensure => file,
+  content => $fqdn
+        }
+ }
 node /^web/ {
   include role::app_server
   }
