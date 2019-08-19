5f6d635f83cbf35e410bfcc4c008ffd601046155
pinejunction <54043253+pinejunction@users.noreply.github.com>
2019-08-19 17:39:28 -0600

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,4 +2,12 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'web.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
+  host {'web.puppet.vm':
+    endure  => present,
+    ip      => '172.18.0.2',
+        }
+   host {'db.puppet.vm':
+      endure  => present,
+      ip      => '172.18.0.3',
+        }
   }
