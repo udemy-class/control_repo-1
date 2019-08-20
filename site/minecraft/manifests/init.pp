a0cf40df388936827425e77e4d35d7eaf1349e05
pinejunction <54043253+pinejunction@users.noreply.github.com>
2019-08-19 21:27:38 -0600

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,6 +1,6 @@
 class minecraft {
   $url = 'https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar'
-  $install_dir = $install_dir
+  $install_dir = "{$install_dir}"
   file {'/opt/minecraft':
   ensure => directory,
         }
