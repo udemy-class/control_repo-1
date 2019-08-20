8555d86c0dbabed841638ac7c2e820f9ca212469
pinejunction <54043253+pinejunction@users.noreply.github.com>
2019-08-19 21:18:16 -0600

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -23,7 +23,7 @@ class minecraft {
     service {'minecraft':
         ensure => running,
         enable => true,
-        require => [Package['java'],File["{$install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']]
+        require => [Package['java'],File["{$install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
             }
      }
         
