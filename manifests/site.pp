429d3f97fefa058baf7f7c02d8df3450cdc92e99
pinejunction <54043253+pinejunction@users.noreply.github.com>
2019-08-19 20:48:06 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,7 +7,7 @@ node 'master.puppet.vm' {
   content => "Welcome to ${fqdn}\n",
         }
  }
-node 'minetest.puppet.vm {
+node 'minetest.puppet.vm' {
     include role::minecraft_server
     }
 node /^web/ {
