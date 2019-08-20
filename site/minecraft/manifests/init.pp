60f56dec76d69a50284bc5fb48a08148ebb53d78
pinejunction <54043253+pinejunction@users.noreply.github.com>
2019-08-19 21:06:13 -0600

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -5,6 +5,7 @@ class minecraft {
         file {'/opt/minecraft/server.jar':
         ensure => file,
         source => 'https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar',
+        before => Service ['minecraft'],
               }
         package {'java':
         ensure => present,
@@ -20,6 +21,7 @@ class minecraft {
     service {'minecraft':
         ensure => running,
         enable => true,
+        require => [Package['java'],File['/opt/minecraft/eula.txt'],File['/etc/systemd/system/minecraft.service']]
             }
      }
         
